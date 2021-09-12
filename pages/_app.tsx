234c441363ca261bcc015ba723c7aa996c09f017
Francisco <javiermartincuenca@gmail.com>
2021-09-12 15:18:53 +0200

    setup tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import 'tailwindcss/tailwind.css';
+import '@assets/main.css';
 
 const Noop: FC = ({ children }) => <>{children}</>;
 
@@ -7,7 +9,7 @@ function MyApp({
   Component,
   pageProps,
 }: AppProps & { Component: { Layout: FC } }) {
-  const { Layout } = Component ?? Noop;
+  const Layout = Component.Layout ?? Noop;
   return (
     <Layout>
       <Component {...pageProps} />
