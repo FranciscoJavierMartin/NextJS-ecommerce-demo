8fc5bb126391577d0809869587a8bdb94cad749f
Francisco <javiermartincuenca@gmail.com>
2021-09-14 21:14:56 +0200

    Style product card

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Product } from '@common/types/product';
 import { PRODUCTS_ROUTE } from 'constants/routes';
+import styles from './ProductCard.module.css';
 
 const placeholderImage = '/product-image-placeholder.svg';
 
@@ -13,15 +14,17 @@ interface ProductCardProps {
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
     <Link href={`/${PRODUCTS_ROUTE}/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={styles.root}>
+        <div className={styles.productBg} />
+        <div className={styles.productTag}>
+          <h3 className={styles.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14 $</span>
+          <span className={styles.productPrice}>14 $</span>
         </div>
         {product.images && (
           <Image
+            className={styles.productImage}
             alt={product.name}
             src={product.images[0].url ?? placeholderImage}
             height={540}
