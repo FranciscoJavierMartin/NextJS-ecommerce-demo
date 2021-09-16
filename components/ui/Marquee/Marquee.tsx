662ce8a7e1b6503aa9ba7ac91091008a1747f652
Francisco <javiermartincuenca@gmail.com>
2021-09-16 22:00:17 +0200

    Add marquee variant

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 import Ticker from 'react-ticker';
+import cn from 'classnames';
 
 import styles from './Marquee.module.css';
 
-const Marquee: FC = ({ children }) => {
+interface MarqueeProps {
+  variant?: 'primary' | 'secondary';
+}
+
+const Marquee: FC<MarqueeProps> = ({ children, variant = 'primary' }) => {
+  const rootClassName = cn(styles.root, {
+    [styles.secondary]: variant === 'secondary',
+  });
+
   return (
-    <div className={styles.root}>
+    <div className={rootClassName}>
       <Ticker offset={80}>
         {() => <div className={styles.container}>{children}</div>}
       </Ticker>
