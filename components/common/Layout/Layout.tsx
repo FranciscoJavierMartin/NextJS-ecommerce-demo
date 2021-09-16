ea29bf8c3137d3ce8a71ca4ef70f41660442a80e
Francisco <javiermartincuenca@gmail.com>
2021-09-16 22:11:18 +0200

    Footer added

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 import style from './Layout.module.css';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div className={style.root}>
       <main className='fit'>{children}</main>
+      <Footer />
     </div>
   );
 };
