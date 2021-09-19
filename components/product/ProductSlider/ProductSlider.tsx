af44fca23441a51e04c69537b951b0ee7fc37236
Francisco <javiermartincuenca@gmail.com>
2021-09-19 12:40:09 +0200

    Add buttons to slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,17 +1,30 @@
-import React, { FC, Children, isValidElement } from 'react';
+import React, { FC, Children, isValidElement, useState } from 'react';
 import { useKeenSlider } from 'keen-slider/react';
+import cn from 'classnames';
+
 import styles from './ProductSlider.module.css';
 
 const ProductSlider: FC = ({ children }) => {
-  const [sliderRef, _] = useKeenSlider<HTMLDivElement>({
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
     initial: 0,
     loop: true,
-    slideChanged(s) {},
+    slideChanged(s) {
+      setCurrentSlide(s.details().relativeSlide);
+    },
   });
 
   return (
     <div className={styles.root}>
       <div ref={sliderRef} className='keen-slider h-full transition-opacity'>
+        <button
+          onClick={slider?.prev}
+          className={cn(styles.leftControl, styles.control)}
+        />
+        <button
+          onClick={slider?.next}
+          className={cn(styles.rightControl, styles.control)}
+        />
         {Children.map(children, (child) =>
           isValidElement(child)
             ? {
