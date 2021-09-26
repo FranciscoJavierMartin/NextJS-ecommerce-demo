3572e7848b5a856d79351ba646bf422f5f815a33
Francisco <javiermartincuenca@gmail.com>
2021-09-26 14:04:44 +0200

    Show items on Navbar

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -2,21 +2,31 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import { useUI } from '@contexts/ui/UIWrapper';
 import { WISHLIST_ROUTE } from 'constants/routes';
-import useCart from '@common/cart/useCart';
+import useCart from '@framework/cart/useCart';
 
 import { Bag as Cart, Heart } from '@components/icons';
 
 import styles from './Usernav.module.css';
+import { LineItem } from '@common/types/cart';
 
 const Usernav: FC = () => {
   const { openSidebar } = useUI();
   const { data } = useCart();
+
+  const itemsCount =
+    data?.lineItems.reduce(
+      (acc: number, lineItem: LineItem) => acc + lineItem.quantity,
+      0
+    ) ?? 0;
 
   return (
     <nav>
       <ul className={styles.list}>
         <li className={styles.item}>
           <Cart onClick={openSidebar} />
+          {itemsCount > 0 && (
+            <span className={styles.bagCount}>{itemsCount}</span>
+          )}
         </li>
         <li className={styles.item}>
           <Link href={`/${WISHLIST_ROUTE}`}>
